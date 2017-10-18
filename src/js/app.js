94c99706418190f66e1c1c22260ed0f5a10814c6
Hai Nguyen <phihai91@Hai.local>
2017-10-18 16:10:01 +0700

    load img

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
-// $('.carousel-item').addClass('animated fadeIn');
+$('.carousel-item').addClass('animated fadeIn');
 
-// $('.js--wp-service').waypoint(function(direction){
-//     $('.js--wp-service').addClass('animated fadeInUp')
-// }, {
-//     offset: '50%'
-// });
+$('.js--wp-service').waypoint(function(direction){
+    $('.js--wp-service').addClass('animated fadeInUp')
+}, {
+    offset: '50%'
+});
 
-// $('.js--wp-gallery').waypoint(function(direction){
-//     $('.js--wp-gallery').addClass('animated fadeIn')
-// }, {
-//     offset: '50%'
-// });
+$('.js--wp-gallery').waypoint(function(direction){
+    $('.js--wp-gallery').addClass('animated fadeIn')
+}, {
+    offset: '50%'
+});
 
-$("img").one("load", function() {
-    $(this).addClass('animated fadeIn');
-})
+// $("img").one("load", function() {
+//     $(this).addClass('animated fadeIn');
+// })
