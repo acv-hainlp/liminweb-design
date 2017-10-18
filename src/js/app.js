e6ac54ec67c3117c45a03ecc2a6b6da0ac948bd3
Hai Nguyen <phihai91@Hai.local>
2017-10-18 15:55:13 +0700

    revert load img

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,13 @@
-// $('.carousel-item').addClass('animated fadeIn');
+$('.carousel-item').addClass('animated fadeIn');
 
-// $('.js--wp-service').waypoint(function(direction){
-//     $('.js--wp-service').addClass('animated fadeInUp')
-// }, {
-//     offset: '50%'
-// });
+$('.js--wp-service').waypoint(function(direction){
+    $('.js--wp-service').addClass('animated fadeInUp')
+}, {
+    offset: '50%'
+});
 
-// $('.js--wp-gallery').waypoint(function(direction){
-//     $('.js--wp-gallery').addClass('animated fadeIn')
-// }, {
-//     offset: '50%'
-// });
-
-$("img").one("load", function() {
-    $(this).addClass('animated fadeIn');
-})
+$('.js--wp-gallery').waypoint(function(direction){
+    $('.js--wp-gallery').addClass('animated fadeIn')
+}, {
+    offset: '50%'
+});
