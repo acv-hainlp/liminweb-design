186753b5369e841e3de5f190f503b4484c7dcc8f
Hai Nguyen <phihai91@Hai.local>
2017-10-18 13:25:32 +0700

    animation add

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+$('.carousel-item').addClass('animated fadeIn');
+
+$('.js--wp-service').waypoint(function(direction){
+    $('.js--wp-service').addClass('animated fadeInUp')
+}, {
+    offset: '50%'
+});
+
+$('.js--wp-gallery').waypoint(function(direction){
+    $('.js--wp-gallery').addClass('animated fadeIn')
+}, {
+    offset: '50%'
+})
